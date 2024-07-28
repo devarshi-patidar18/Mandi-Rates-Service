53d682f3d1bf312d97c7f5504d28a3d055367198
devarshi-patidar1812 <devarshi330@gmail.com>
2024-07-28 22:54:05 +0530

    Initial Commit

---
+++ src/main/java/com/webscrap/Mandi_Rates_Service/MandiRatesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.webscrap.Mandi_Rates_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandiRatesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandiRatesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webscrap/Mandi_Rates_Service/MandiRatesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webscrap.Mandi_Rates_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandiRatesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
